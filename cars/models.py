193f80ed3b42e59d6932b4c6826dd172f9b14d5d
Tiago Yamamoto <japa@rede5.com.br>
2021-02-11 15:20:06 -0300

    makemigrations app cars

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from buyers.models import Buyer
+
 
 # Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    code = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return f"{self.name}-{self.price}-{self.buyer}"
