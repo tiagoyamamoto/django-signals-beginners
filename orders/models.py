829b5ed25d27fc4525745241be5467443138f3eb
Tiago Yamamoto <japa@rede5.com.br>
2021-02-12 15:46:09 -0300

    add makemigrations e signals app orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from cars.models import Car
+
 
 # Create your models here.
+
+class Order(models.Model):
+    name = models.CharField(max_length=200)
+    cars = models.ManyToManyField(Car)
+    total = models.PositiveIntegerField(blank=True, null=True)
+    total_price = models.PositiveIntegerField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.name)
